f46ef6915d75c7653207a296ca8eef0b3680f99f
Lawrence <@iamlawrence.in>
2026-03-15 22:56:23 +0530

    01-folder

---
+++ 01_folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+      <h1 id='child' >Lawrence</h1>
+      <h2 id='child2'>lawrence</h2>
+    </div>
+    </>
+    
+  )
+}
+
+export default App

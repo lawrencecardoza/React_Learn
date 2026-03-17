9e7a652561a079a87837555ad9a64d5e38b80c0f
Lawrence <@iamlawrence.in>
2026-03-17 20:30:34 +0530

    updated

---
+++ 02_components/01_folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+      <h1 id='child' >Lawrence</h1>
+      <h2 id='child2'>lawrence</h2>
+    </div>
+
+    <Card />
+    </>
+    
+  )
+}
+
+export default App

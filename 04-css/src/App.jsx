ebfaaf7d25b4cedd85069340f205be8a68bd07c3
Lawrence <@iamlawrence.in>
2026-03-22 21:27:18 +0530

    initial commit

--- 04-css/src/App.jsx
+++ 04-css/src/App.jsx
@@ -1,34 +1,38 @@
 import React from 'react'
-import Header from './components/header/Header'
-import Button from './components/button/Button'
+
+const App = () => {
+  return (
+    <div className='container'>
+
+      <nav>
+        <a className="logo" href="#">
+          <div className="logo-icon">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <polygon points="8,2 14,13 2,13" fill="white" />
+            </svg>
+          </div>
+          <span className="logo-text">MyBrand</span>
+        </a>
+
+        <ul className='nav-links'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Project</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
 
 
-// const App = () => {
-//   return (
-//    <div>
-//     <Header />
-//     <Button />
-//    </div>
-//   )
-// }
+        <button className='nav-btn'>Hire me</button>
+      </nav>
 
+      <section className='hero'>
+        <h1>Hi , I'm Full Stack Developer </h1>
+        <p>React aur Css se Clean UI banata hoon.</p>
+        <button className='hero-btn'>Apply Me</button>
+      </section>
 
-// // Inline CSS 
-// function App(){
-//   return <h1 style={{color:"red"}}>Lawrence</h1>
-// }
-
-// External CSS
-function App(){
-  const style = {
-    color:"blue",
-    fontSize:"20px"
-  };
-
-  
-return <h1 style={style}>Hi React</h1>
-
+    </div>
+  )
 }
 
 export default App
-
